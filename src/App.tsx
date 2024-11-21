b016c823b54d4a15212449f2e898851ad138e093
odinokun <odinokun@gmail.com>
2024-11-21 17:22:04 -0500

    Add task management to Todolist component with dynamic rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import { Todolist } from './components/Todolist';
 import './App.css';
 
+export type TaskType = {
+  id: number;
+  title: string;
+  isDone: boolean;
+};
+
 function App() {
+  const tasks1: TaskType[] = [
+    { id: 1, title: 'HTML&CSS', isDone: true },
+    { id: 2, title: 'JS', isDone: true },
+    { id: 3, title: 'React', isDone: false },
+  ];
+
+  const tasks2: TaskType[] = [
+    { id: 1, title: 'Milk', isDone: true },
+    { id: 2, title: 'Bread', isDone: false },
+    { id: 3, title: 'Beer', isDone: true },
+  ];
+
   return (
     <div className=''>
-      <Todolist title={'What to learn'} />
-      <Todolist title={'What to buy'} />
+      <Todolist title={'What to learn'} tasks={tasks1} />
+      <Todolist title={'What to buy'} tasks={tasks2} />
     </div>
   );
 }
--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
+import { TaskType } from '../App';
 import { Button } from './Button';
 
 type PropsType = {
   title: string;
+  tasks: TaskType[];
 };
 
-export const Todolist: FC<PropsType> = ({ title, ...restProps }) => {
+export const Todolist: FC<PropsType> = ({ title, tasks, ...restProps }) => {
   return (
     <div>
       <h3>{title}</h3>
@@ -22,15 +24,13 @@ export const Todolist: FC<PropsType> = ({ title, ...restProps }) => {
       </div>
 
       <ul>
-        <li>
-          <input type='checkbox' checked={true} /> <span>HTML&CSS</span>
-        </li>
-        <li>
-          <input type='checkbox' checked={true} /> <span>JS</span>
-        </li>
-        <li>
-          <input type='checkbox' checked={false} /> <span>React</span>
-        </li>
+        {tasks.map(t => (
+          <li key={t.id}>
+            <Button name={'del'} onClick={() => {}} />
+            <input type='checkbox' checked={t.isDone} />
+            <span>{t.title}</span>
+          </li>
+        ))}
       </ul>
     </div>
   );
